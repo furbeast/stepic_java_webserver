78feb92f7544dafef6957458fcfd77ac25f13359
slonn91 <slonn91@mail.ru>
2020-06-04 00:42:03 +0300

    Add handler /mirror

--- L1.1 Simple web server/src/main/java/main/Main.java
+++ L1.1 Simple web server/src/main/java/main/Main.java
@@ -4,6 +4,7 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import servlets.AllRequestsServlet;
+import servlets.RequestMirror;
 
 /**
  * @author v.chibrikov
@@ -15,14 +16,18 @@ import servlets.AllRequestsServlet;
 public class Main {
     public static void main(String[] args) throws Exception {
         AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
+        RequestMirror requestMirror = new RequestMirror();
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.addServlet(new ServletHolder(allRequestsServlet), "/*");
+        context.addServlet(new ServletHolder(requestMirror), "/mirror");
 
         Server server = new Server(8080);
         server.setHandler(context);
 
         server.start();
         server.join();
+
+
     }
 }
---
+++ L1.1 Simple web server/src/main/java/servlets/RequestMirror.java
@@ -0,0 +1,34 @@
+package servlets;
+
+import templater.PageGenerator;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RequestMirror extends HttpServlet {
+
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException {
+
+//        Map<String, Object> pageVariables = createPageVariablesMap(request);
+//        pageVariables.put("message", "");
+
+//        response.getWriter().println(PageGenerator.instance().getPage("page1.html", pageVariables));
+        response.getWriter().println(request.getParameter("key"));
+
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+
+    }
+
+    private static Map<String, Object> createPageVariablesMap(HttpServletRequest request) {
+        Map<String, Object> pageVariables = new HashMap<>();
+        pageVariables.put("parameters", request.getParameterMap().toString());
+        return pageVariables;
+    }
+}
